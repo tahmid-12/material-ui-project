b4720ddd160782e27445520e8f07a91f153cadf4
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-08-26 18:58:25 +0600

    Some package installation.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { Button,Typography  } from "@mui/material";
-import { AddIcon, SettingsIcon } from '@mui/icons-material';
+// import { AddIcon } from '@mui/icons-material';
+// import { AddIcon } from '@mui/icons-material';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   return (
     <div>
       <Button variant="text">Text</Button>
-      <Button startIcon={<SettingsIcon />} variant="contained" color="secondary" size="large">Contained</Button>
       <Button startIcon={<AddIcon />} variant="contained" color="success" size="large">Success</Button>
       <Button variant="outlined" disabled>Outlined</Button>
       <Typography variant="h1" component="p">
