aee3a63cf0f7f708e6f139e43d25d909cd4e75eb
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-08-26 20:56:38 +0600

    Some practise

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import { Button,Typography  } from "@mui/material";
-// import { AddIcon } from '@mui/icons-material';
-// import { AddIcon } from '@mui/icons-material';
-import AddIcon from '@mui/icons-material/Add';
 
 function App() {
+
+  
+
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button startIcon={<AddIcon />} variant="contained" color="success" size="large">Success</Button>
-      <Button variant="outlined" disabled>Outlined</Button>
-      <Typography variant="h1" component="p">
-            This is H2 heading
-      </Typography>
+      
     </div>
   );
 }
---
+++ src/Practise.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button,styled,Typography  } from "@mui/material";
+// import { AddIcon } from '@mui/icons-material';
+// import { AddIcon } from '@mui/icons-material';
+import AddIcon from '@mui/icons-material/Add';
+
+function Practise() {
+
+   const BlueButton = styled(Button)({
+        backgroundColor: 'skyblue',
+        color: '#888',
+        margin: 5,
+        '&:hover':{
+          backgroundColor: 'lightblue'
+        },
+        '&:disabled':{
+          backgroundColor: 'gray',
+          color: 'white'
+        }
+  });
+
+  return (
+    <div>
+        <Button variant="text">Text</Button>
+        <Button startIcon={<AddIcon />} variant="contained" color="success" size="large">Success</Button>
+        <Button variant="outlined" disabled>Outlined</Button>
+        <Typography variant="h1" component="p">
+                This is H2 heading
+        </Typography>
+
+        <BlueButton>
+            My Unique Button
+        </BlueButton>
+
+        <BlueButton>
+            My Unique Button Anther one
+        </BlueButton>
+    </div>
+  )
+}
+
+export default Practise
