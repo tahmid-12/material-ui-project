f4e1dd6896f98726d24989396ca9403b41d0027f
Md. Tahmid Chowdhury <mdtahmidchowdhury12@gmail.com>
2022-09-10 18:33:26 +0600

    Theme Provider Practise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+// import Practise from "./Practise";
+// import { ThemeProvider } from "@mui/material";
+// import { theme } from './theme';
+import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
 
 function App() {
-
   
-
   return (
     <div>
-      
+      {/* <ThemeProvider theme={theme}>
+        <Practise />
+      </ThemeProvider> */}
+      <Navbar />
+      <Feed />
+      <Rightbar />
     </div>
   );
 }
